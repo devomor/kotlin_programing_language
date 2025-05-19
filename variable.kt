52caf81e7052091b9f5af0b56c5ae91f05d30dfd
MD Omar Faruk <87695345+devomor@users.noreply.github.com>
2025-05-19 11:53:34 +0600

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,14 @@
+fun main() {
+   // kotlin variable
+    val a: Int = 12
+    val b: String = "Omar"
+    val c: Double = 12.0
+    val d: Boolean = false
+    val e: Char = 'A'
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+}
+
